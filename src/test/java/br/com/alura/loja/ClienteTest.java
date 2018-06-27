38bb6f4b76635672e4e0875e799e0bf7a49fd967
Maiquel Knechtel <maiquelknechtel@MacBook-Pro-de-Maiquel.local>
2018-06-27 20:34:40 -0300

    adicionado post

--- src/test/java/br/com/alura/loja/ClienteTest.java
+++ src/test/java/br/com/alura/loja/ClienteTest.java
@@ -7,8 +7,7 @@ import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
 
 import org.glassfish.grizzly.http.server.HttpServer;
-import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
-import org.glassfish.jersey.server.ResourceConfig;
+
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
